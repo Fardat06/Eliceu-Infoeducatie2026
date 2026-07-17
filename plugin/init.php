a06f0e38d42f90c4e2d4a4b527035caf52f4d8db
layla-237 <xlaylacoconutx@gmail.com>
2026-07-17 17:14:21 +0300

    Update init.php

--- plugin/init.php
+++ plugin/init.php
@@ -1,15 +1,14 @@
 <?php
-   //session_start();
     require_once('config.php');  
     ini_set('default_charset', 'UTF-8');
     include 'connect.php';  
     
-if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 1800)) { // last request was more than 30 minutes ago
-    session_unset();     // unset $_SESSION variable for the run-time 
-    session_destroy();   // destroy session data in storage
+if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 1800)) { 
+    session_unset();     
+    session_destroy();   
     header('Location: /index.php');
 }else{
-    $_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
+    $_SESSION['LAST_ACTIVITY'] = time(); 
     if (isset($_SESSION['Language'])) {
         $Language = $_SESSION['Language'] . '.php';
         $langFile =  'languages' . $Language;
@@ -20,10 +19,8 @@ if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 
     if (file_exists($langFile)) {
         include $langFile;
     } else {
-        // fallback so $pageTitle=lang(...) below doesn't fatal on undefined function
         include __DIR__ . '/languages/en.php';
     }
-   // $pageTitle=lang($pageTitle1);
     include 'function.php';
 }
 ?>
