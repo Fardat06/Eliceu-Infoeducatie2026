c88db7060a55d96ef6896670481ccbc8283e7b46
Fardat06 <fatimatimi08@gmail.com>
2026-07-17 17:16:17 +0300

    Update function.php

--- admin/function.php
+++ admin/function.php
@@ -11,7 +11,6 @@
      
      function sanitize_output($buffer) {
 
-    // Searching textarea and pre
     preg_match_all('#\<textarea.*\>.*\<\/textarea\>#Uis', $buffer, $foundTxt);
     preg_match_all('#\<pre.*\>.*\<\/pre\>#Uis', $buffer, $foundPre);
 
@@ -19,12 +18,12 @@
     $buffer = str_replace($foundTxt[0], array_map(function($el){ return '<textarea>'.$el.'</textarea>'; }, array_keys($foundTxt[0])), $buffer);
     $buffer = str_replace($foundPre[0], array_map(function($el){ return '<pre>'.$el.'</pre>'; }, array_keys($foundPre[0])), $buffer);
 
-    // your stuff
+
     $search = array(
-        '/\>[^\S ]+/s',  // strip whitespaces after tags, except space
-        '/[^\S ]+\</s',  // strip whitespaces before tags, except space
-        '/(\s)+/s' ,      // shorten multiple whitespace sequences
-        '/<!--(.|\s)*?-->/' // Remove HTML comments
+        '/\>[^\S ]+/s',  
+        '/[^\S ]+\</s',
+        '/(\s)+/s' ,      
+        '/<!--(.|\s)*?-->/' 
     );
 
     $replace = array(
@@ -35,7 +34,7 @@
 
     $buffer = preg_replace($search, $replace, $buffer);
 
-    // Replacing back with content
+ 
     $buffer = str_replace(array_map(function($el){ return '<textarea>'.$el.'</textarea>'; }, array_keys($foundTxt[0])), $foundTxt[0], $buffer);
     $buffer = str_replace(array_map(function($el){ return '<pre>'.$el.'</pre>'; }, array_keys($foundPre[0])), $foundPre[0], $buffer);
 
