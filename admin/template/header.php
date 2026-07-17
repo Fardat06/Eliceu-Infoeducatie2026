8673de25085b6f4d3b8b245dbeedcf808cc1a5fd
Fardat06 <fatimatimi08@gmail.com>
2026-07-17 17:23:53 +0300

    Update header.php

--- admin/template/header.php
+++ admin/template/header.php
@@ -9,13 +9,10 @@ $css  = '';
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Dashboard - E-Liceu</title>
-    <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
-    <!-- Google Icons -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <!-- CSS -->
     <link rel="stylesheet" href="layout/css/style.css" />
   </head>
   <body>
